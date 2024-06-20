d2610d67234329433430cd3a1812c055256a59bb
Raniia Davurova <134239457+raniiyya@users.noreply.github.com>
2024-06-20 16:13:13 +0000

    m

---
+++ ft_strspn.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+
+size_t	ft_strspn(const char *s, const char *accept)
+{
+    int i;
+    int j = 0;
+    int count;
+
+    if (!s || !accept)
+        return 0;
+    while (s[j])
+    {
+        i = 0;
+        count = 0;
+        while (accept[i] != '\0')
+        {
+            if (s[j] == accept[i])
+                count++;
+            i++;
+        }
+        j++;
+    }
+    return count;
+}
+
+#include <stdio.h>
+
+int main(void)
+{
+    char s[] = "rani";
+    char accept[] = "davur";
+
+    printf("%zu", ft_strspn(s, accept));
+}
