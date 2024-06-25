e3c31057402433a1bee897d1d6739538e63552f7
Raniia Davurova <134239457+raniiyya@users.noreply.github.com>
2024-06-25 18:22:20 +0000

    m

--- inter.c
+++ inter.c
@@ -1,34 +1,31 @@
-int main(int ac, char **av) {
-    int i;
-    int j;
-    int k;
-    int count;
-    
-    if (ac != 3) {
-        write(1, "\n", 1);
-        return 0;
-    }
-    i = 0;
-    j = 0;
-    while (av[1][i]) {
-        while (av[2][j]) {        // abscde axcbxcxdxsffex
-            if (av[1][i] == av[2][j]) {
-                k = 0;
-                count = 0;
-                while (k < j) {
-                    if (av[2][j] == av[2][k]) {
-                        count++;
-                    }
-                    k++;
-                }
-                if (count == 0) {
-                    write(1, &av[1][i], 1);
-                }
-                break;
+#include <unistd.h>
+
+void    inter(char *s1, char *s2)
+{
+    int met[128] = {0}; //for checking the doubles
+    int i = 0; //for iterating over av1
+    int j = 0; //for iterating over av2
+
+    while (s1[i])
+    {
+        j = 0;
+        while (s2[j])
+        {
+            if(s1[i] == s2[j] && met[(int)s1[i]] == 0)
+            {
+                write(1, &s1[i], 1);
+                met[(int)s1[i]] = 1;
+                continue;
             }
             j++;
         }
         i++;
     }
-    write(1, "\n", 1);
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 3)
+        return (1);
+    inter(argv[1], argv[2]);
 }
