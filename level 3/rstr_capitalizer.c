eaa730abc9a040cc4e04b805ae4b30df87ffcabe
Raniia Davurova <134239457+raniiyya@users.noreply.github.com>
2024-07-01 22:59:53 +0000

    updates

--- level 3/rstr_capitalizer.c
+++ level 3/rstr_capitalizer.c
@@ -1,32 +1,59 @@
 #include <unistd.h>
 
-static void rstr_capitalizer(char *str)
-{
-	int i = 0;
-
-	while (str[i]) {
-		if (str[i] >= 'A' && str[i] <= 'Z')
-			str[i] += 32;
-		if ((str[i] >= 'a') && (str[i + 1] <= ' ' || str[i + 1] == '\t' || str[i + 1] == '\0'))
-			str[i] -= 32;
-		write(1, &str[i], 1);
-		i++;
-	}
-	write(1, "\n", 1);
-}
-
 int main(int ac, char *av[])
 {
-	int i = 1;
+	int i = 0;
+	int j = 1;
 
 	if (ac == 1)
 	{
 		write(1, "\n", 1);
 		return 0;
 	}
-	while (i < ac) {
-		rstr_capitalizer(av[i]);
-		i++;
+	while (av[j]) {
+		i = 0;
+		while (av[j][i]) {
+			if (av[j][i] >= 'A' && av[j][i] <= 'Z')
+				av[j][i] += 32;
+			if ((av[j][i] >= 'a') && (av[j][i + 1] <= ' ' || av[j][i + 1] == '\t' || av[j][i + 1] == '\0'))
+				av[j][i] -= 32;
+			write(1, &av[j][i], 1);
+			i++;
+		}
+		write(1, "\n", 1);
+		j++;
 	}
-	return 0;
 }
+
+// #include <unistd.h>
+
+// static void rstr_capitalizer(char *str)
+// {
+// 	int i = 0;
+
+// 	while (str[i]) {
+// 		if (str[i] >= 'A' && str[i] <= 'Z')
+// 			str[i] += 32;
+// 		if ((str[i] >= 'a') && (str[i + 1] <= ' ' || str[i + 1] == '\t' || str[i + 1] == '\0'))
+// 			str[i] -= 32;
+// 		write(1, &str[i], 1);
+// 		i++;
+// 	}
+// 	write(1, "\n", 1);
+// }
+
+// int main(int ac, char *av[])
+// {
+// 	int i = 1;
+
+// 	if (ac == 1)
+// 	{
+// 		write(1, "\n", 1);
+// 		return 0;
+// 	}
+// 	while (i < ac) {
+// 		rstr_capitalizer(av[i]);
+// 		i++;
+// 	}
+// 	return 0;
+// }
