cec88cdf7acdd6e269222f3ed62405e8c55f6440
Raniia Davurova <134239457+raniiyya@users.noreply.github.com>
2024-07-02 19:23:45 +0000

    updates

---
+++ level 3/str_capitalizer.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+
+int main(int ac, char *av[])
+{
+	int i;
+	int j = 1;
+
+	if (ac == 1) {
+		write(1, "\n", 1);
+		return 0;
+	}
+	while (av[j]) {
+		i = 0;
+		while (av[j][i] == ' ' && av[j][i] == '\t')
+			i++;
+		while (av[j][i]) {
+			if (av[j][0] >= 'a' && av[j][0] <= 'z')
+				av[j][i] -= 32;
+			if (av[j][i - 1] == ' ' || av[j][i] == '\t') {
+				if (av[j][i] >= 'A' && av[j][i] <= 'Z')
+					av[j][i] += 32;
+				if (av[j][i - 1] != ' ' || av[j][i - 1] != '\t'
+					|| (av[j][i] >= 'a' && av[j][i] <= 'z'))
+					av[j][i] -= 32;
+			}
+			write(1, &av[j][i], 1);
+			i++;
+		}
+		j++;
+		write(1, "\n", 1);
+	}
+}
