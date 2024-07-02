08396985a783b4738b9fe90ef2c48706f68ee698
Raniia Davurova <134239457+raniiyya@users.noreply.github.com>
2024-07-02 20:08:40 +0000

    reordered and changed IFs and loops

--- level 3/str_capitalizer.c
+++ level 3/str_capitalizer.c
@@ -9,20 +9,18 @@ int main(int ac, char *av[])
 		write(1, "\n", 1);
 		return 0;
 	}
+	
 	while (av[j]) {
 		i = 0;
-		while (av[j][i] == ' ' && av[j][i] == '\t')
+		while (av[j][i] == ' ' && av[j][i] == '\t') 
 			i++;
+		if (av[j][0] >= 'a' && av[j][0] <= 'z')
+			av[j][i] -= 32;
 		while (av[j][i]) {
-			if (av[j][0] >= 'a' && av[j][0] <= 'z')
+			if ((av[j][i - 1] == ' ' || av[j][i - 1] == '\t') && av[j][i] >= 'A' && av[j][i] <= 'Z')
+				av[j][i] += 32;
+			else if ((av[j][i - 1] != ' ' || av[j][i - 1] != '\t') && av[j][i] >= 'a' && av[j][i] <= 'z')
 				av[j][i] -= 32;
-			if (av[j][i - 1] == ' ' || av[j][i] == '\t') {
-				if (av[j][i] >= 'A' && av[j][i] <= 'Z')
-					av[j][i] += 32;
-				if (av[j][i - 1] != ' ' || av[j][i - 1] != '\t'
-					|| (av[j][i] >= 'a' && av[j][i] <= 'z'))
-					av[j][i] -= 32;
-			}
 			write(1, &av[j][i], 1);
 			i++;
 		}
