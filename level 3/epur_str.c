6b26674ae48f552176dfd5368e6118bd439b3db4
Raniia Davurova <134239457+raniiyya@users.noreply.github.com>
2024-07-04 14:17:47 +0000

    update

--- level 3/epur_str.c
+++ level 3/epur_str.c
@@ -1,26 +1,26 @@
 #include <unistd.h>
 
-int main(int ac, char *av[])
-{
-	int i = 0;
+int main(int ac, char **av) {
+    int i = 0;
 
-	if (ac != 2) {
-		write(1, "\n", 1);
-		return 0;
-	}
-	while (av[1][i] == ' ' || av[1][i] == '\t')
-		i++;
-	while (av[1][i]) {
-		if ((av[1][i] == ' ' || av[1][i] == '\t') && av[1][i + 1] == '\0')
-			av[1][i] = '\0';
-		write(1, &av[1][i], 1);
-		if (av[1][i] != ' ' || av[1][i] != '\t')
-			i++;
-		if (av[1][i] == ' ' || av[1][i] == '\t') {
-			while (av[1][i] == ' ' || av[1][i] == '\t')
-				i++;
-			write(1, " ", 1);
-		}
-	}
-	write(1, "\n", 1);
+    if (ac != 2) {
+        write(1, "\n", 1);
+        return 0;
+    }
+    while (av[1][i] == ' ' || av[1][i] == '\t')
+        i++;
+    while (av[1][i]) {
+        if ((av[1][i] == ' ' || av[1][i] == '\t') && av[1][i + 1] == '\0')
+            av[1][i] = '\0';
+        write(1, &av[1][i], 1);
+        if (av[1][i] != ' ' && av[1][i] != '\t') {
+            i++;
+        } else {
+            while (av[1][i] == ' ' || av[1][i] == '\t') {
+                i++;
+            }
+        }
+    }
+    write(1, "\n", 1);
+    return 0;
 }
