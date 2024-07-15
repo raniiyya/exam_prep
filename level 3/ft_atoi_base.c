9c56b2a451aa824fbb746ba44b04915475cc183c
Raniia Davurova <134239457+raniiyya@users.noreply.github.com>
2024-07-15 11:59:57 +0000

    corrected

--- level 3/ft_atoi_base.c
+++ level 3/ft_atoi_base.c
@@ -1,7 +1,15 @@
+#include <stdio.h>
+
+char to_upper(char c) {
+    if (c >= 'a' && c <= 'f')
+        return (c - 32);
+    return c;
+}
+
 int     ft_atoi_base(const char *str, int str_base)
 {
-    int result;
-    int sign = -1;
+    int result = 0;
+    int sign = 1;
 
     if (*str == '-') {
         while (*str == '-')
@@ -9,13 +17,22 @@ int     ft_atoi_base(const char *str, int str_base)
         sign = -1;
     }
     while (*str) {
-        if (*str >= '0' && *str <= '9')
+        char upperchar = to_upper(*str);
+        printf("%c", upperchar);
+        if (str_base > 10) {
+            if (!((upperchar >= '0' && upperchar <= '9') 
+            || (upperchar >= 'A' && upperchar <= (str_base - 10 + 'A') ))) {
+                break;
+            }
+        }
+        if (upperchar >= '0' && upperchar <= '9')
             result = result * str_base + *str - '0';
-        if (*str >= 'A' && *str <= 'F')
-            result = result * str_base + *str - 'A' + 10;
-        if (*str >= 'a' && *str <= 'f')
-            result = result * str_base + *str - 'a' + 10;
+        if (upperchar >= 'A' && upperchar <= 'F')
+            result = result * str_base + upperchar - 'A' + 10;
         str++;
     }
     return result * sign;
+}
+int main() {
+    printf("%d", ft_atoi_base("abc", 16));
 }
