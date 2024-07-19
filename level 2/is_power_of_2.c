289c2f408a84bb1ae0897b50de54cc576d19672e
Raniia Davurova <134239457+raniiyya@users.noreply.github.com>
2024-07-19 09:49:54 +0000

    + explanation

--- level 2/is_power_of_2.c
+++ level 2/is_power_of_2.c
@@ -2,5 +2,5 @@ int is_power_of_2(unsigned int n)
 {
     if (n == 0)
         return 0;
-    return (n & (n - 1)) == 0;
+    return (n & (n - 1)) == 0; //if we compare a num to a num - 1 and the comparison is 0000 => it's power of 2
 }
