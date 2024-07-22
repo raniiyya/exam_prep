549daa0f20f8b760b746cc8c1b1a90ebb7d2a326
Raniia Davurova <134239457+raniiyya@users.noreply.github.com>
2024-07-22 10:05:34 +0000

    corrected + comments

--- level 4/rev_wstr.c
+++ level 4/rev_wstr.c
@@ -1,28 +1,27 @@
 #include <unistd.h>
 
-int main(int ac, char *av[])
+int main(int ac, char **av)
 {
-	int i;
-	int count = 0;
+	int i = 0;
+	int len = 0;
 
-	if (ac != 2)
-	{
+	if (ac != 2) {
 		write(1, "\n", 1);
-		return (0);
+		return 0;
 	}
-	while (av[1][count] != '\0')
-		count++;
-	while (count != -1) {
-		if (av[1][count - 1] == '\t' || av[1][count - 1] == ' ' || count == 0) { //the next char is '\t' or ' ' or 0
-			i = count;
-			while (av[1][i] != '\0' && av[1][i] != ' ' && av[1][i] != '\t') { //printing the word
+	while (av[1][len])
+		len++;
+	while (len != -1) {
+		if (av[1][len - 1] == ' ' || av[1][len - 1] == '\t' || len == 0) { //the next char is space/tab/at index 0
+			i = len;
+			while (av[1][i] != ' ' && av[1][i] != '\t' && av[1][i]) { //printing the word
 				write(1, &av[1][i], 1);
 				i++;
 			}
-		if (av[1][count] == ' ')
-			write(1, " ", 1);
 		}
-		count--;
+		if (av[1][len] == ' ') //printing spaces
+			write(1, " ", 1);
+		len--;
 	}
-    write(1, "\n", 1);
+	write(1, "\n", 1);
 }
